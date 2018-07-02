b96c5795c3f53e558f37a6e099a720b385093d03
Bastien NICOLAS <bnicobas@gmail.com>
2018-07-02 15:06:51 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    title: 'WEB SERVICES STATUS GUI',
+    title: 'WEB SERVICES STATUS GUI', // Web interface title
     port: 3000,
-    http_timeout: 10, // seconds
+    http_timeout: 10, // http request timeout in seconds
     ping_timeout: 10,
     services: [
         {
             type: 'ping', // ping, http or text (only text without status)
-            host: 'skeelapp.com', // or '8.8.8.8',
+            host: 'skeelapp.com', // or '8.8.8.8' (useless if type text)
             name: 'Skeel server', // text displayed beside the status
             services: [ // you can do service trees to infinity
                 {
